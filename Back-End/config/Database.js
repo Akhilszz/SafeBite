0ebb81068d7da6c2c3568d652af91d0f1d56844d
Akhilszz <akhilk.20047@gmail.com>
2024-09-16 15:13:01 +0530

    updations

--- Back-End/config/Database.js
+++ Back-End/config/Database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const Connect = async () => {
     try {
-       await mongoose.connect('mongodb://localhost:27017/FoodSafety')
+        await mongoose.connect('mongodb+srv://new-user-123:new-user-123@cluster0.9wpyu.mongodb.net/FoodSafety?retryWrites=true&w=majority&appName=Cluster0')
             .then(() => {
                 console.log('DB connected successfully');
             })
